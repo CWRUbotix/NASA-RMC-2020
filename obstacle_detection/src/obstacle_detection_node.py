a6e734587f8014d411d10f3843eddf71f3042750
Dylan Plummer <dylanmplummer@gmail.com>
2019-11-23 13:48:36 -0500

    Add local occupancy grid message

--- obstacle_detection/src/obstacle_detection_node.py
+++ obstacle_detection/src/obstacle_detection_node.py
@@ -7,6 +7,7 @@ import math
 import rospy
 import math
 import matplotlib
+
 matplotlib.use('Agg')  # necessary when plotting without $DISPLAY
 import numpy as np
 import pandas as pd
@@ -14,21 +15,27 @@ import matplotlib.pyplot as plt
 from scipy import signal
 from scipy.spatial.transform import Rotation as R
 
-from nav_msgs.msg import Odometry
+from std_msgs.msg import Header
+from nav_msgs.msg import MapMetaData, OccupancyGrid
+from geometry_msgs.msg import Pose, Point, Quaternion
 
 from pylibfreenect2 import Freenect2, SyncMultiFrameListener
 from pylibfreenect2 import FrameType, Registration, Frame
 from pylibfreenect2 import createConsoleLogger, setGlobalLogger
 from pylibfreenect2 import LoggerLevel
+
 try:
     from pylibfreenect2 import OpenGLPacketPipeline
+
     pipeline = OpenGLPacketPipeline()
 except:
     try:
         from pylibfreenect2 import OpenCLPacketPipeline
+
         pipeline = OpenCLPacketPipeline()
     except:
         from pylibfreenect2 import CpuPacketPipeline
+
         pipeline = CpuPacketPipeline()
 print("Packet pipeline:", type(pipeline).__name__)
 # Create and set logger
@@ -81,26 +88,27 @@ class ObstacleDetectionNode:
         self.localization_topic = 'odometry/filtered_map'
         self.viz_dir = 'obstacle_viz/'
         self.data_dir = 'saved_frames/'
-        #camera information based on the Kinect v2 hardware
+        # camera information based on the Kinect v2 hardware
         self.CameraParams = {
-          "cx":254.878,
-          "cy":205.395,
-          "fx":365.456,
-          "fy":365.456,
-          "k1":0.0905474,
-          "k2":-0.26819,
-          "k3":0.0950862,
-          "p1":0.0,
-          "p2":0.0,
+            "cx": 254.878,
+            "cy": 205.395,
+            "fx": 365.456,
+            "fy": 365.456,
+            "k1": 0.0905474,
+            "k2": -0.26819,
+            "k3": 0.0950862,
+            "p1": 0.0,
+            "p2": 0.0,
         }
         # Kinect's physical orientation in the real world.
         self.CameraPosition = {
-            "x": 0, # actual position in meters of kinect sensor relative to the viewport's center.
-            "y": 0, # actual position in meters of kinect sensor relative to the viewport's center.
-            "z": 0, # height in meters of actual kinect sensor from the floor.
-            "roll": 0, # angle in degrees of sensor's roll (used for INU input - trig function for this is commented out by default).
-            "azimuth": 0, # sensor's yaw angle in degrees.
-            "elevation": -30, # sensor's pitch angle in degrees.
+            "x": 0,  # actual position in meters of kinect sensor relative to the viewport's center.
+            "y": 0,  # actual position in meters of kinect sensor relative to the viewport's center.
+            "z": 0,  # height in meters of actual kinect sensor from the floor.
+            "roll": 0,
+            # angle in degrees of sensor's roll (used for INU input - trig function for this is commented out by default).
+            "azimuth": 0,  # sensor's yaw angle in degrees.
+            "elevation": -30,  # sensor's pitch angle in degrees.
         }
 
         print('Booting up node...')
@@ -134,7 +142,6 @@ class ObstacleDetectionNode:
                 os.remove(f)
         except Exception as e:
             print(e)
-        
 
     def project_point_cloud_onto_plane(self, xyz_arr, resize_factor=10, cropping=500):
         print(xyz_arr.shape)
@@ -166,18 +173,45 @@ class ObstacleDetectionNode:
         return np.column_stack((z.ravel() / scale, R.ravel(), -C.ravel()))
 
     def detect_obstacles_from_above(self, frame):
-        frame[frame < 1000] = 0
         xyz_arr = self.depth_matrix_to_point_cloud(frame)
         # point_height = xyz_arr[]
         print(xyz_arr[..., 2].mean())
-        rocks = self.project_point_cloud_onto_plane(xyz_arr[xyz_arr[..., 2] >= self.ground_plane_height + self.tolerance])
-        holes = self.project_point_cloud_onto_plane(xyz_arr[xyz_arr[..., 2] < self.ground_plane_height - self.tolerance])
+        rocks = self.project_point_cloud_onto_plane(
+            xyz_arr[xyz_arr[..., 2] >= self.ground_plane_height + self.tolerance])
+        holes = self.project_point_cloud_onto_plane(
+            xyz_arr[xyz_arr[..., 2] < self.ground_plane_height - self.tolerance])
         rock_grid = self.gridify(rocks, (self.grid_size, self.grid_size))
         hole_grid = self.gridify(holes, (self.grid_size, self.grid_size))
         obs_grid = np.maximum(rock_grid, hole_grid)
         kernel = np.ones((self.kernel_size, self.kernel_size))
 
         occupancy_grid = signal.convolve2d(obs_grid, kernel, boundary='symm', mode='same')
+        occupancy_grid /= np.max(occupancy_grid)  # ensure values are 0-1
+
+        header = Header()
+        header.stamp = rospy.Time.now()
+        header.frame_id = 'odom'
+
+        map_meta_data = MapMetaData()
+        map_meta_data.map_load_time = rospy.Time.now()
+        map_meta_data.resolution = 0.15  # each cell is 15cm
+        map_meta_data.width = self.grid_size
+        map_meta_data.height = self.grid_size
+        map_meta_data.origin = Pose(Point(0, 0, 0),
+                                    Quaternion(0, 0, 0, 1))
+
+        grid_msg = OccupancyGrid()
+        grid_msg.header = header
+        grid_msg.info = map_meta_data
+        grid_msg.data = list(np.round(occupancy_grid.flatten() * 100))
+
+        try:
+            pub = rospy.Publisher('local_occupancy_grid', OccupancyGrid, queue_size=1)
+            rospy.loginfo(grid_msg)
+            pub.publish(grid_msg)
+        except rospy.ROSInterruptException as e:
+            print(e.getMessage())
+            pass
 
         if self.save_imgs:
             fig = plt.figure(figsize=(15, 5))
@@ -231,10 +265,8 @@ class ObstacleDetectionNode:
             color_frame = registered.asarray(np.uint8)
             if self.save_data:
                 np.save('%s/%d.npy' % (self.data_dir, len(os.listdir(self.data_dir))), depth_frame.asarray())
-                cv2.imwrite('%s/%d.png' % (self.data_dir + 'color', len(os.listdir(self.data_dir + 'color'))), color.asarray())
-                #np.save('%s/%d.npy' % (self.data_dir + 'color', len(os.listdir(self.data_dir + 'color'))), color_frame)
-                #np.save('%s/%d.npy' % (self.data_dir + 'localization', len(os.listdir(self.data_dir + 'localization'))), np.array(self.robot_x[-1], self.robot_y[-1], self.robot_pitch[-1]))
-
+                cv2.imwrite('%s/%d.png' % (self.data_dir + 'color', len(os.listdir(self.data_dir + 'color'))),
+                            color.asarray())
 
             img = depth_frame.asarray(np.float32)
             img = cv2.flip(img, 1)
@@ -251,11 +283,9 @@ class ObstacleDetectionNode:
 
 
 if __name__ == '__main__':
-	try:
-	    obstacle_detection = ObstacleDetectionNode()
-	    print('Listening for frames...')
-	    #sub = rospy.Subscriber(obstacle_detection.localization_topic, Odometry, obstacle_detection.localization_listener, queue_size=1)
-	    #rospy.spin()
-	    obstacle_detection.listen_for_frames()
-	except rospy.exceptions.ROSInterruptException:
-		pass
+    try:
+        obstacle_detection = ObstacleDetectionNode()
+        print('Listening for frames...')
+        obstacle_detection.listen_for_frames()
+    except rospy.exceptions.ROSInterruptException:
+        pass
