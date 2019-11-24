659fee3148948a511f330b68f495fef2c0f2c3b5
Seohyun Jung <sxj393@case.edu>
2019-11-24 17:29:16 -0500

    fixed some issues

--- autonomy/src/Automodule/autonomy_node.py
+++ autonomy/src/Automodule/autonomy_node.py
@@ -2,7 +2,7 @@
 import rospy
 from autonomy.msg import goToGoal
 
-goal_pub = rospy.Publisher("goToGoal", goToGoal, queue_size=10)
+goal_pub = rospy.Publisher("transit_command", goToGoal, queue_size=10)
 
 def on_shut_down():
     global goal_pub
--- autonomy/src/Automodule/robot_state.py
+++ autonomy/src/Automodule/robot_state.py
@@ -3,11 +3,30 @@
 import rospy
 from nav_msgs.msg import Odometry
 from nav_msgs.msg import OccupancyGrid
+import hci.msg as hci
+import autonomy.msg as aut
 from autonomy.srv import RobotState, RobotStateResponse
 
 odometry = None
 occupancy_grid = None
 service = None
+sensors = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
+
+sensormap = {
+    2  : 0,  # depWinchEncoder
+    3  : 1,  # bcBeltEncoder
+    4  : 2,  # bcTranslationEncoder
+    5  : 3,  # bcAttitudePortPot
+    6  : 4,  # bcAttitudeStarboardPot
+    9  : 5,  # depLoadCell
+    10 : 6,  # excLoadCell
+    23 : 7,  # depLowerLimit
+    24 : 8,  # depUpperLimit
+    25 : 9,  # excForeLimit
+    26 : 10, # excAftLimit
+    27 : 11, # bcLowerLimit
+    28 : 12  # bcUpperLimit
+}
 
 def update_odometry(msg):
     global odometry
@@ -17,13 +36,21 @@ def update_occupancy_grid(msg):
     global occupancy_grid
     occupancy_grid = msg
 
+def update_sensors(msg):
+    global sensors, sensormap
+    if msg.sensorID in sensormap.keys():
+        sensors[sensormap[msg.sensorID]] = msg.value
+
 def subscribe():
+    rospy.Subscriber("hci/sensorValue", hci.sensorValue, update_sensors)
     rospy.Subscriber("odometry/filtered_map", Odometry, update_odometry)
     #rospy.Subscriber("", OccupancyGrid, ) #todo
 
 def send_robot_state(req):
     global odometry, occupancy_grid
-    return autonomy.srv.RobotStateResponse(time=rospy.get_rostime(), odometry=odometry, grid=occupancy_grid)
+    (s0, s1, s2, s3, s4, s5, s6, s7, s8, s9, s10, s11, s12) = tuple(sensors)
+    sensor_msg = aut.sensorValue(s0, s1, s2, s3, s4, s5, s6, s7, s8, s9, s10, s11, s12)
+    return RobotStateResponse(time=rospy.get_rostime(), odometry=odometry, grid=occupancy_grid, sensors=sensor_msg)
 
 def on_shut_down():
     global service
@@ -33,7 +60,7 @@ def main():
     global service
     rospy.init_node("robot_state")
     subscribe()
-    #service = rospy.Service("robot_state", autonomy.srv.RobotState, send_robot_state)
+    service = rospy.Service("robot_state", RobotState, send_robot_state)
     rospy.spin()
 
 
