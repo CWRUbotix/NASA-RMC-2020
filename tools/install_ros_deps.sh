bf6beaa5e64ab3498175ca53c77356c28139d1e9
Ethan Frank <edf32@case.edu>
2020-09-01 17:44:03 -0400

    Add a few helpful setup scripts

---
+++ tools/install_ros_deps.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+set -e # Exit if any command fails
+
+PACKAGES=(
+    # Kalman filters for robot localization
+    ros-$ROS_DISTRO-robot-localization
+
+    # rqt provides useful visualizer tools
+    ros-$ROS_DISTRO-rqt
+    ros-$ROS_DISTRO-rqt-common-plugins
+    ros-$ROS_DISTRO-rqt-robot-plugins
+
+    # Plotjuggler is a very cool plotting software
+    ros-$ROS_DISTRO-plotjuggler
+
+    # Used to drive simulated robot
+    ros-$ROS_DISTRO-ros-controllers
+
+    # Gazebo is the simulation used with ros
+    # These packages depend on Gazebo and will install it automatically
+    ros-$ROS_DISTRO-gazebo-ros-pkgs
+    ros-$ROS_DISTRO-gazebo-ros-control
+)
+
+sudo apt update # Make sure package list is up to date
+sudo apt install ${PACKAGES[@]} # Install all ros packages
