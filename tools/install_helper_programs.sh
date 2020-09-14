3aca6d02f8b35d57ae5942a1fa705d9b6835eb74
InvincibleRMC <rmc@carlstrom.com>
2020-09-14 18:04:16 -0400

    python-catkin-tools added to helper_programs.sh

--- tools/install_helper_programs.sh
+++ tools/install_helper_programs.sh
@@ -3,6 +3,9 @@
 set -e # Exit if any command fails
 
 PACKAGES=(
+    #Allows Use of catkin Build and other catkin tools
+    python-catkin-tools
+
     # Tells you cpu temps (run `sensors`)
     lm-sensors
 
