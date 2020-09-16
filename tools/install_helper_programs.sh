0e5fa54148831d906e43f18a9ed933870b151c24
InvincibleRMC <rmc@carlstrom.com>
2020-09-16 18:01:06 -0400

    call to install python packages and update pip

--- tools/install_helper_programs.sh
+++ tools/install_helper_programs.sh
@@ -21,3 +21,7 @@ PACKAGES=(
 
 sudo apt update # Make sure package list is up to date
 sudo apt install ${PACKAGES[@]} # Install all packages
+
+ pip3 install --user --upgrade pip # Updates Pip
+ pip3 install -r requirements.txt # Installs require python packages
+
