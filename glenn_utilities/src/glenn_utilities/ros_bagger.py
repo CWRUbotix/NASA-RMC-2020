481c922b202c907ecb09b0c978569e57fbc1fc93
Ethan Frank <edf32@case.edu>
2020-09-21 17:47:59 -0400

    Change bagging save folder structure

--- glenn_utilities/src/glenn_utilities/ros_bagger.py
+++ glenn_utilities/src/glenn_utilities/ros_bagger.py
@@ -11,11 +11,14 @@ class RosBagger():
         rospy.init_node('bagging_node')
 
         bag_topics = rospy.get_param("~topics")
-        bag_dir = os.path.expandvars("$HOME/glenn_bags")
-        today = datetime.datetime.now().strftime("%Y-%m-%d-%H-%M-%S")
+
+        today = datetime.datetime.now().strftime("%Y_%m_%d")
+        now = datetime.datetime.now().strftime("%Y_%m_%d_%H_%M_%S")
+
+        bag_dir = os.path.expandvars("$HOME/glenn_bags" + "/" + today)
 
         if not os.path.exists(bag_dir):
-            os.mkdir(bag_dir)
+            os.makedirs(bag_dir)
 
         # Record specific bag topics, save bag to bag_dir
-        self.bag_process = subprocess.Popen(["rosbag", "record", "-O", bag_dir + "/" + today + ".bag"] + bag_topics)
+        self.bag_process = subprocess.Popen(["rosbag", "record", "-O", bag_dir + "/" + now + ".bag"] + bag_topics)
