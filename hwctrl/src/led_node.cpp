ad42026d83a2d2296844253e3cb1f08cf8067584
nnh12 <nnh12@case.edu>
2021-02-13 17:01:55 -0500

    ROS LED Node

---
+++ hwctrl/src/led_node.cpp
@@ -0,0 +1,42 @@
+#include "ros/ros.h"
+#include "hwctrl/CanFrame.h"
+#include "hwctrl/LED.h"
+
+int r, g, b;
+
+bool receiveColors(hwctrl::LED::Request &req , hwctrl::LED::Response &res )
+{
+    ROS_INFO("reqest: r=%ld, g = %ld, b = %ld",(long int) req.r, (long int) req.g, (long int) req.b);
+    r = req.r;
+    g = req.g;
+    b = req.b;
+    return true;
+}
+
+int main(int argc, char **argv)
+{
+     ros::init(argc, argv, "led_node");
+     ros::NodeHandle n;
+     ros::Publisher m_can_rx_pub = n.advertise<hwctrl::CanFrame>("can_frames_tx", 2);
+     ros::Rate loop_rate(10);
+     ros::ServiceServer service = n.advertiseService("led_node_service", receiveColors);
+     ROS_INFO("Ready to recieve led colors");
+
+    while(ros::ok())
+    {
+        hwctrl::CanFrame msg;
+        msg.can_id = 10;
+        msg.can_dlc= 3;
+
+        msg.data[0] = r;
+        msg.data[1] = b;
+        msg.data[2] = g;
+        m_can_rx_pub.publish(msg);
+        loop_rate.sleep();
+    }
+
+    return 0;
+
+ }
+
+
