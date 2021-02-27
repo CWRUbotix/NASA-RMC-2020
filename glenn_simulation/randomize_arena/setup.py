27df986b8487b85460f01ddbea9c076fa2782e8d
InvincibleRMC <rmc@carlstrom.com>
2021-02-27 12:09:57 -0500

    switching glenn_simulation to group of packages

--- glenn_simulation/randomize_arena/setup.py
+++ glenn_simulation/randomize_arena/setup.py
@@ -7,9 +7,7 @@ from catkin_pkg.python_setup import generate_distutils_setup
 
 # fetch values from package.xml
 setup_args = generate_distutils_setup(
-    packages=['glenn_simulation',
-              'glenn_simulation.glenn_description',
-              'glenn_simulation.randomize_arena'],
+    packages=['randomize_arena'],
     package_dir={'': 'src'},
 )
 
