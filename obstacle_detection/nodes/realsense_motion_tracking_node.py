7673632540cadb065888d707ef3da9808e7118c3
Ethan Frank <edf32@case.edu>
2021-06-14 17:52:22 -0400

    Delete unused nodes

--- obstacle_detection/nodes/realsense_motion_tracking_node.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-import rospy
-from obstacle_detection.realsense_motion_tracking import RealSenseIMU
-
-if __name__ == '__main__':
-    try:
-        imu_node = RealSenseIMU()
-    except rospy.exceptions.ROSInterruptException:
-        pass
