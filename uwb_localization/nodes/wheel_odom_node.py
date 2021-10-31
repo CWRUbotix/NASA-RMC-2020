b516330c7f8740509251a0ad683f529720718073
Glenn 2017 <rmc170@case.edu>
2021-10-31 13:19:23 -0400

    sbg now publishes standard imu messages

--- uwb_localization/nodes/wheel_odom_node.py
+++ uwb_localization/nodes/wheel_odom_node.py
@@ -19,7 +19,7 @@ from nav_msgs.msg import Odometry
 from hwctrl.msg import Encoders
 
 
-class IMU:
+class WHEELODOMETRY:
     def __init__(self):
         self.viz_dir = 'imu_plots'
         self.save_plots = False
@@ -41,10 +41,8 @@ class IMU:
                                         rospy.get_param('imu_angular_vel_z_offset')]
         self.acceleration_offset = [0, 0, 0]
 
-        self.wheel_pub = rospy.Publisher('wheel', Odometry, queue_size=10)
-        self.imu_pub = rospy.Publisher('imu/data', Imu, queue_size=10)
-
-        rospy.Subscriber('imu/data_raw', Imu, self.receive_imu)
+        self.wheel_pub = rospy.Publisher('glenn_base/odom', Odometry, queue_size=10)
+        
         rospy.Subscriber('glenn_base/encoders', Encoders, self.receive_encoders)
 
         os.makedirs(self.viz_dir, exist_ok=True)
@@ -57,70 +55,12 @@ class IMU:
 
         rospy.spin()
 
-    def receive_imu(self, msg):
-        msg.linear_acceleration.x += self.acceleration_offset[0]
-        msg.linear_acceleration.y += self.acceleration_offset[1]
-        msg.linear_acceleration.z += self.acceleration_offset[2]
-
-        msg.angular_velocity.x += self.angular_velocity_offset[0]
-        msg.angular_velocity.y += self.angular_velocity_offset[1]
-        msg.angular_velocity.z += self.angular_velocity_offset[2]
-
-        self.publish_imu_msg(msg)
-
-        # self.orientation_plot = np.vstack((self.orientation_plot, self.orientation))
-        self.angular_velocity_plot = np.vstack((self.angular_velocity_plot, self.angular_velocity))
-        self.acceleration_plot = np.vstack((self.acceleration_plot, self.acceleration))
-
-        print('X: %.4f' % np.var(self.angular_velocity_plot[..., 0]), 'Y: %.4f' % np.var(self.angular_velocity_plot[..., 1]), 'Z: %.4f' % np.var(self.angular_velocity_plot[..., 2]))
-
-        if self.orientation_plot.shape[0] % 5 == 0 and self.save_plots:
-            fig = plt.figure(figsize=(15, 5))
-            ax = plt.subplot(131)
-            ax.plot(self.orientation_plot[..., 0], label='x')
-            ax.plot(self.orientation_plot[..., 1], label='y')
-            ax.plot(self.orientation_plot[..., 2], label='z')
-            ax.set_title('Orientation')
-            ax.legend(loc='best')
-
-            ax = plt.subplot(132)
-            ax.plot(self.angular_velocity_plot[..., 0], label='x')
-            ax.plot(self.angular_velocity_plot[..., 1], label='y')
-            ax.plot(self.angular_velocity_plot[..., 2], label='z')
-            ax.set_title('Angular Velocity')
-            ax.legend(loc='best')
-
-            ax = plt.subplot(133)
-            ax.plot(self.acceleration_plot[..., 0], label='x')
-            ax.plot(self.acceleration_plot[..., 1], label='y')
-            ax.plot(self.acceleration_plot[..., 2], label='z')
-            ax.set_title('Acceleration')
-            ax.legend(loc='best')
-
-            fig.savefig(self.viz_dir + '/imu_%d.png' % (len(os.listdir(self.viz_dir))))
-            plt.close()
-
-    def publish_imu_msg(self, msg):
-        # orientation_quat = R.from_euler('xyz', self.orientation, degrees=True).as_quat()
-        # orientation_cov = np.ravel(np.eye(3) * 1e-9)
-        # quat_msg = Quaternion(orientation_quat[0], orientation_quat[1], orientation_quat[2], orientation_quat[3])
-
-        # Keep everything the same but update covariances
-        angular_vel_cov = np.ravel(np.eye(3) * 1e-4)
-        accel_cov = np.ravel(np.eye(3) * 1e-4)
-
-        msg.angular_velocity_covariance = angular_vel_cov
-        msg.linear_acceleration_covariance = accel_cov
-
-        try:
-            self.imu_pub.publish(msg)
-        except rospy.ROSInterruptException as e:
-            rospy.logerr(e.getMessage())
-
+    
+    
     def receive_encoders(self, msg):
         header = Header()
         header.stamp = rospy.Time.now()
-        header.frame_id = 'base_link'
+        header.frame_id = 'odom'
 
         point_msg = Point(0, 0, 0)
         orientation_quat = R.from_euler('xyz', [0, 0, 0]).as_quat()
@@ -144,7 +84,7 @@ class IMU:
 
         stamped_msg = Odometry()
         stamped_msg.header = header
-        stamped_msg.child_frame_id = 'base_link'
+        stamped_msg.child_frame_id = 'base_footprint'
         stamped_msg.pose = pose_with_cov
         stamped_msg.twist = twist_with_cov
 
@@ -155,4 +95,4 @@ class IMU:
 
 
 if __name__ == '__main__':
-    imu_node = IMU()
+    wheel_odom_node = WHEELODOMETRY()
